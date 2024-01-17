3c696359947d7b7cc2a2c70da400a01d3289f0fd
gmrv <gmrv04@gmail.com>
2024-01-17 21:10:18 +0300

    fix[defbot]: has_alphanumeric_words

--- common/utils.py
+++ common/utils.py
@@ -6,6 +6,7 @@ def is_contains_stop_words(input_string, stop_words_list):
     return any(word in input_string for word in stop_words_list)
 
 def contain_alphanumeric(word):
+    if len(word) < 5: return False
     return any(char.isalpha() for char in word) and any(char.isdigit() for char in word)
 
 def has_alphanumeric_words(text):
